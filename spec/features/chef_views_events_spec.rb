75d135d51b574040e19e2617951aa135e0622a5c
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-07-09 08:50:40 -0400

    forgot to work in branch.

---
+++ spec/features/chef_views_events_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+feature 'user_views_events', %Q(
+  As a chef
+  I want I to view a list of events
+  so that I can apply for them.
+  ) do
+  # Acceptance Criteria:
+    # The chef must sign in.
+    # The chef is provided with a list of events that are sorted by location.
+    # The chef can click on the event to view its details.
+    # The details should provide the event details and user email.
+
+  let(:user) { FactoryGirl.create(:user) }
+  # let(:event) { FactoryGirl.create(:event) }
+
+  scenario 'user views all events' do
+    login_as user
+    event = FactoryGirl.create(:event)
+
+    visit  events_path
+
+    save_and_open_page
+    expect(page).to have_content event.title
+    expect(page).to have_content event.city
+    expect(page).to have_content event.start_time
+  end
+
+  scenario 'user views a single event' do
+    login_as user
+    event = FactoryGirl.create(:event)
+    # visit event_path #vs.
+    visit events_path
+
+    click_link("#{event.title}")
+    expect(page).to have_content event.title
+    expect(page).to have_content event.description
+    expect(page).to have_content event.user.email
+  end
+end
