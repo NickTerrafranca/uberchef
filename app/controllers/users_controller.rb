f03e1ce35f7f1c795f967e15a07a195d5278432f
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-07-10 20:46:33 -0400

    Have user info displaying properly on user show page   but need event and bid to display dynamically.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,1 +1,13 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @user = current_user
+    @event = Event.find(current_user.id)
+    # binding.pry
+    # if @user.bids.any?#exists?
+    #   @bid = Bid.find(current_user.id)
+    # end
+  end
+
+end
--- spec/features/user_creates_event_spec.rb
+++ spec/features/user_creates_event_spec.rb
@@ -29,7 +29,7 @@ feature 'user creates a new event', %Q(
     fill_in 'Event start time', with: event.start_time
     fill_in 'Event end time', with: event.end_time
     fill_in 'Expected number of guests', with: event.guest_count
-    fill_in 'Budget per person', with: event.budget
+    fill_in 'Budget price per person', with: event.budget
     fill_in 'Event details', with: event.description
 
     click_on 'Submit'
