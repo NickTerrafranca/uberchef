59bc4e6c366781129d529fac053f1877713528b5
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-07-11 16:56:14 -0400

    Formatted event time.   added files to Gemfile   started process fo deploying to heroku.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,5 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
-        # binding.pry
-
-    # @event = Event.find(current_user)
-    # binding.pry
-    # if @user.bids.any?#exists?
-    #   @bid = Bid.find(current_user.id)
-    # end
   end
-
 end
