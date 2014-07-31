8e470af5f407c86f92e83cb552e5a9a8847d3d52
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-07-31 05:53:41 -0400

    A small refactor to messages model.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ActiveRecord::Base
   belongs_to :sender, class_name: 'User'
-  belongs_to :receiver, class_name: 'User'#, foreign_key: :user_id
-  validates :sender_id, presence: true
-  validates :receiver_id, presence: true
+  belongs_to :receiver, class_name: 'User'
+  validates :sender, presence: true
+  validates :receiver, presence: true
   validates :body, presence: true
 end
