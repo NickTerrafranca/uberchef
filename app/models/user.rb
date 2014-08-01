e62ba11cab81cc26d5436c550d2d25056bce3850
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-08-01 15:49:13 -0400

    Finished bid bug fix.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ActiveRecord::Base
   has_many :events_bid_on, through: :bids, source: :event
   has_many :events_hosted, class_name: 'Event'
 
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
-  # validates :email, presence: true
-  # validates :city, presence: true
-  # validates :state, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
