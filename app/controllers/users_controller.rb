241b383b7bf840e9a1d267bf66a63fb8fa7a2897
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-08-11 10:33:21 -0400

    Made the messages page link to both the message and the sender.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  def index
-    @user = User.order('last_name')
-  end
+  # def index
+  #   @user = User.order('last_name')
+  # end
 
   def show
     @user = User.find(params[:id])
