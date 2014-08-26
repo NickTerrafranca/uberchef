618e99b108c1cd069e6f242bb634dbfb0910b7dc
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-08-26 08:15:21 -0400

    Have a sql queri that will do the job.

--- app/models/message.rb
+++ app/models/message.rb
@@ -14,7 +14,12 @@ class Message < ActiveRecord::Base
   end
 
   def message_thread(params)
-    Message.where('sender_id = ? OR receiver_id = ?', current_user.id, params)
+    Message.find_by_sql(
+      "SELECT * FROM messages WHERE sender_id = current_user OR sender_id = #{params} AND receiver_id = current_user OR receiver_id = #{params};"
+      )
+
+    # Message.where('sender_id = ? OR receiver_id = ?', current_user.id, params) and ('receiver_id = ? OR receiver_id = ?', current_user.id, params)
+
   end
 
 end
