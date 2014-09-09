80818e80a1efd30d2f6225e54bedfe1d152cb181
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-09-09 07:58:57 -0400

    Re-factored method to find associated users.   Now more idiomatic and uses fewer queries.

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -2,9 +2,11 @@ class Bid < ActiveRecord::Base
   belongs_to :applicant, class_name: 'User', foreign_key: :user_id
   belongs_to :event
 
-  # bid should have only one user_id to limit a user to one bid per evet per user (uniqueness: true)
   validates :user_id, presence: true
   validates :event_id, presence: true
   validates :message, presence: true
   validates :amount, presence: true, numericality: { greater_than: 0 }
 end
+
+
+# add_index :bids, [:user_id, :event_id], unique: true
