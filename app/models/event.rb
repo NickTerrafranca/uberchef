db8af098cacdd7d15c787fba0f389a104d782f4a
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-09-23 19:55:51 -0400

    Added the new search code. Not working yet.

--- app/models/event.rb
+++ app/models/event.rb
@@ -21,7 +21,14 @@ class Event < ActiveRecord::Base
   end
 
   def self.search(query)
-    where("title ilike ?", "%#{query}%").order('title')
+    # where("title ilike ?", "%#{query}%").order('title')
+    if query.match(/^(?:(A[KLRZ]|C[AOT]|D[CE]|FL|GA|HI|I[ADLN]|K[SY]|LA|M[ADEINOST]|N[CDEHJMVY]|O[HKR]|P[AR]|RI|S[CD]|T[NX]|UT|V[AIT]|W[AIVY]))$/)
+      where("state = ?", "%#{query}%").order('state')
+    elsif where("city = ?", "%#{query}%").any?
+      where("city = ?", "%#{query}%").order('city')
+    else
+      where("title ilike ?", "%#{query}%").order('title')
+    end
   end
 
   def full_address
