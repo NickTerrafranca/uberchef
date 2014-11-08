f76ee65e0ef384391c531081ba6c18718fa1f55f
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-11-08 12:53:30 -0500

    Setting up new tests.

---
+++ spec/features/user_views_profile_spec.rb
@@ -0,0 +1,26 @@
+# require 'rails_helper'
+
+# feature 'User views profile page', %Q(
+#   As a user, I want to view a profile page
+#   so that I can have an overview
+#   of my information and activity.
+#   ) do
+
+#   let(:user) { FactoryGirl.create(:user) }
+
+#   before :each do
+#     login_as user
+#   end
+
+
+
+#   scenario 'User views their own profile page with events and bids' do
+
+#   end
+
+#   scenario "User views someone else's profile page with events and bids" do
+
+#   end
+
+
+# end
