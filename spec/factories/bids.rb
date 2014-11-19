90c7745d9b5847a9d3101d954417efce9a7dde96
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-11-18 20:24:40 -0500

    Fleshed bid test.   Some aspects are not working yet but that will cange tomorrow.

--- spec/factories/bids.rb
+++ spec/factories/bids.rb
@@ -1,6 +1,9 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :bid do
+    # below was an attempt to provide the necessary associated event_id and user_id
+    # association :event, factory: :event, strategy: :create
+    event
+    message "This is a bid on an event"
+    amount 100
   end
 end
