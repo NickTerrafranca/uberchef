d6f186ff335816f8f269175d006d9e9241525d39
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-12-19 14:29:22 -0500

    Fleshed out the remaining test scenarios

--- spec/features/user_edits_profile_spec.rb
+++ spec/features/user_edits_profile_spec.rb
@@ -5,21 +5,33 @@ feature 'user edits their profile information', %Q(
   I want to be able to edit my account profile
   So that I can keep my info up to date.) do
 
-  scenario 'User properly fills in required information', focus: true do
-    user = FactoryGirl.create(:user)
+  let(:user) { FactoryGirl.create(:user) }
+
+  scenario 'User updates profile and properly fills in required information', focus: true do
     login_as user
     visit edit_user_registration_path
-    # save_and_open_page
 
     fill_in 'Address', with: '11235 New Address'
     fill_in 'City', with: 'New City'
     fill_in 'State', with: 'New State'
     fill_in 'Current password', with: user.password
     click_button 'Update'
-    save_and_open_page
 
     expect(page).to have_content('You updated your account successfully.')
     expect(page).to_not have_content("can't be blank")
   end
+
+  scenario 'User updates profile does not to enter confirmation password' do
+  end
+
+  scenario 'User changes password with required information' do
+  end
+
+  scenario 'User changes password without entering confirmation password' do
+  end
+
+  scenario 'User deletes the account' do
+  end
+
 end
 
