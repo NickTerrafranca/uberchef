6fb627e5369ad0ecdd1d5bc897c8532b0117232d
Nicholas Terrafranca <nterrafranca@gmail.com>
2015-01-06 20:35:22 -0500

    Fixed travis... maybe

---
+++ spec/features/user_views_all_messages_spec.rb
@@ -0,0 +1,15 @@
+# require 'rails_helper'
+
+# feature 'User views a list of all the messages threads',
+#   %Q(As a User, I want to view a list of all the messages threads that I am ap part of
+#   so that I can find the message thread I am looking for.
+#   ) do
+
+#   scenario 'user visits all the messages page' do
+#     user = FactoryGirl.create(:user)
+#     message = FactoryGirl.create(:message)
+#     login_as user
+#     visit messages_path(user)
+
+#   end
+# end
