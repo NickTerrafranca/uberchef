9a629304c061f1d7d4efe7d3701dddd1d8792881
Nicholas Terrafranca <nterrafranca@gmail.com>
2015-01-08 16:37:48 -0500

    Refined a feature test

--- spec/features/user_views_all_messages_spec.rb
+++ spec/features/user_views_all_messages_spec.rb
@@ -5,7 +5,7 @@ feature 'User views a list of all the messages threads',
   so that I can find the message thread I am looking for.
   ) do
 
-  scenario 'user visits all the messages page', focuss: true do
+  scenario "the receiving user can see the sending user's message" do
     sender = FactoryGirl.create(:user)
     receiver = FactoryGirl.create(:user)
 
@@ -19,5 +19,20 @@ feature 'User views a list of all the messages threads',
 
     expect(page).to have_content sender.full_name
     expect(page).to_not have_content receiver.full_name
+  end
+
+  scenario "the sending user can see a message sent to the receiving user" do
+    sender = FactoryGirl.create(:user)
+    receiver = FactoryGirl.create(:user)
+
+    login_as sender
+    visit message_path(receiver)
+    fill_in "Message", with: 'This is a message from sending user'
+    click_on 'Send message'
+
+    visit user_messages_path(sender)
+
+    expect(page).to have_content receiver.full_name
+    expect(page).to_not have_content sender.full_name
   end
 end
