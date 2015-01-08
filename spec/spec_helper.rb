bb73a1b6ff92ad7f72968e5651ee3d0f027253cc
Nicholas Terrafranca <nterrafranca@gmail.com>
2015-01-08 15:46:51 -0500

    Adding Coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,6 +35,9 @@ RSpec.configure do |config|
     config.default_formatter = 'doc'
   end
 
+  require 'coveralls'
+  Coveralls.wear!('rails')
+
   # Print the 10 slowest examples and example groups at the
   # end of the spec run, to help surface which specs are running
   # particularly slow.
